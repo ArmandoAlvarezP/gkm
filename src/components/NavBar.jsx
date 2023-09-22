3a1479cee4f3ad5cbb8ba5dd49ffe141847149b7
AalvarezP <armando.aperez.gk@gmail.com>
2023-09-22 13:46:21 -0600

    Estilos de Navbar terminados

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,12 @@
+import { useState } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 export const NavBar = () => {
+    const [activeLink, setActiveLink] = useState('home');
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    } 
   return (
-    <Navbar className='nav' fixed='top'>
+    <Navbar className='nav' fixed='top' expand='lg'>
     <Container>
       <Navbar.Brand href="#home">
         <img
@@ -11,11 +16,41 @@ export const NavBar = () => {
         />
       </Navbar.Brand>
       <Nav className="me-auto">
-            <Nav.Link className='nav-text' href="#home">¿Quiénes Somos?</Nav.Link>
-            <Nav.Link className='nav-text' href="#servicios">Servicios</Nav.Link>
-            <Nav.Link className='nav-text' href="#educacion">Educación</Nav.Link>
-            <Nav.Link className='nav-text' href="#salud">Salud</Nav.Link>
-            <Nav.Link className='nav-text' href="#contacto">Contacto</Nav.Link>
+            <Nav.Link 
+                className={ activeLink === 'home' ? 'active navbar-link nav-text' : 'navbar-link nav-text' } 
+                href="#home"
+                onClick={() => onUpdateActiveLink('home')}
+            >
+                ¿Quiénes Somos?
+            </Nav.Link>
+            <Nav.Link  
+                className={ activeLink === 'servicios' ? 'active navbar-link nav-text' : 'navbar-link nav-text' } 
+                href="#servicios"
+                onClick={() => onUpdateActiveLink('servicios')}
+            >
+                Servicios
+            </Nav.Link>
+            <Nav.Link 
+                className={ activeLink === 'educacion' ? 'active navbar-link nav-text' : 'navbar-link nav-text' } 
+                href="#educacion"
+                onClick={() => onUpdateActiveLink('educacion')}
+            >
+                Educación
+            </Nav.Link>
+            <Nav.Link 
+                className={ activeLink === 'salud' ? 'active navbar-link nav-text' : 'navbar-link nav-text' } 
+                href="#salud"
+                onClick={() => onUpdateActiveLink('salud')}
+            >
+                Salud
+            </Nav.Link>
+            <Nav.Link 
+                className={ activeLink === 'contacto' ? 'active navbar-link nav-text' : 'navbar-link nav-text' } 
+                href="#contacto"
+                onClick={() => onUpdateActiveLink('contacto')}
+            >
+                Contacto
+            </Nav.Link>
           </Nav>
     </Container>
   </Navbar>
