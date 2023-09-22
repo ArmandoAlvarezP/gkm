5f983d98eb9a60f97c9d7014e1f79457c9f1cf9d
AalvarezP <armando.aperez.gk@gmail.com>
2023-09-22 17:44:08 -0600

    Se agrega video de fondo y animación de texto para el banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+export const Banner = () => {
+
+const toRotate = [ "Ideas", "Soluciones" ];
+const [loopNum, setLoopNum] = useState(0);
+const [isDeleting, setIsDeleting] = useState(false);
+const [text, setText] = useState('');
+const [delta, setDelta] = useState(100 - Math.random() * 100);
+const period = 2000;
+
+useEffect(() => {
+    let ticker = setInterval(() => {
+        tick();
+    }, delta )
+
+    return () => { clearInterval(ticker) }
+}, [text]);
+
+const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length -1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if(isDeleting) {
+        setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if(!isDeleting && updatedText === fullText){
+        setIsDeleting(true);
+        setDelta(period);
+    } else if(isDeleting && updatedText === ''){
+        setIsDeleting(false);
+        setLoopNum(loopNum + 1);
+        setDelta(200);
+    }
+}
+
+return (
+    <section className="banner" id="home">
+        <video src="./video/Video-480.mov" loop="true" autoPlay="true" muted="true"></video>
+        <Container>
+            <Row className="align-items-center">
+                <Col>
+                    <h1>Creamos<span className="wrap"> {text}</span></h1>
+                </Col>
+            </Row>
+        </Container>
+    </section>
+)
+
+}
