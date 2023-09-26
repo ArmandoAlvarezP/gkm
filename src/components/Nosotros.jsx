40401727c2b22d224750298ecd7f8240011ae7a9
AalvarezP <armando.aperez.gk@gmail.com>
2023-09-25 18:11:46 -0600

    Se agrega componente Nosotros

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,8 +1,20 @@
-
+import { Container, Row, Col } from "react-bootstrap"
 export const Nosotros = () => {
   return (
     <>
-        <h3 className="nosotros">Nosotros</h3>
+        <Container className="nosotros">
+            <Row>
+                <Col>
+                    <h3>Nosotros</h3>
+                    <hr className="divider" />
+                    <p>
+                    De Cero en Adelante, S.A. de C.V., marca comercial “Gluon Knowledge México”, somos una empresa mexicana de consultoría integral conformada por un equipo de trabajo con más de 20 años de experiencia en el Sector Público y Privado.
+
+                    Estamos orientados a la modernización institucional con el fin de satisfacer y atender las necesidades de control y administración de los Entes Públicos y Privados, encaminándolos a obtener una mejor gestión de sus recursos, auditabilidad, trazabilidad, transparencia de todas sus operaciones, así como la rendición de cuentas y el cumplimiento de los objetivos para los que fueron creados.
+                    </p>
+                </Col>
+            </Row>
+        </Container>
     </>
   )
 }
