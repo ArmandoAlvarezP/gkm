283aba16cdd9696115ef29756bba152035b4a2cb
AalvarezP <armando.aperez.gk@gmail.com>
2023-09-27 12:03:05 -0600

    Se agrega cambio de estilo al hacer scrroll del Navbar y se agrega componente MisionVision

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
+
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+      const onScroll = () => {
+        if (window.scrollY > 50) {
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
+      }
+      window.addEventListener('scroll', onScroll);
+
+      return () => window.removeEventListener('scroll', onScroll);
+    }, [])
+
     const onUpdateActiveLink = (value) => {
         setActiveLink(value);
     } 
   return (
-    <Navbar className='nav' fixed='top' expand='lg'>
+    <Navbar className={ scrolled ? 'nav scrolled' : 'nav' } fixed='top' expand='lg'>
     <Container>
       <Navbar.Brand href="#home">
         <img
