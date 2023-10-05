28ad00dab27725b5358732743dd10f1a341d8171
AalvarezP <armando.aperez.gk@gmail.com>
2023-10-05 16:44:57 -0600

    Se Añaden primeras 2 tarjetas de capacitación

--- src/components/Capacitacion.jsx
+++ src/components/Capacitacion.jsx
@@ -1,5 +1,6 @@
 import { Col, Container, Row } from "react-bootstrap"
 import { TarjetaCapacitacionR } from "./TarjetaCapacitacionR"
+import { TarjetaCapacitacionL } from "./TarjetaCapacitacionL"
 
 export const Capacitacion = () => {
    
@@ -36,6 +37,23 @@ export const Capacitacion = () => {
                         </ul>
                     </p>
                 </TarjetaCapacitacionR>
+                <TarjetaCapacitacionL 
+                    imgNumber={<img src="/img/2-flag.png" />}
+                    img={<img src="/img/Cursos-Web-fotos2.png" />}
+                >
+                    <h5>Enfoque Social</h5>
+                    <p>
+                        Somos una organización encargada de proporcionar soluciones de capacitación y educación con el compromiso de contribuir al desarrollo económico sostenible por medio de la colaboración con sus empleados, sus familias, la comunidad local y la inclusión laboral de personas con discapacidad, participando en proyectos de responsabilidad social en alianza con organizaciones civiles, impulsando un programa de becas para personas de escasos recursos.
+                    </p>
+                    <h5>Enfoque Internacional</h5>
+                    <p>
+                        Contamos con alianzas internacionales que nos permite integrar un equipo consolidado de consultores, expositores, y maestros con amplia experiencia, con la finalidad de brindar a nuestros clientes, soluciones a sus necesidades de capacitación.
+                    </p>
+                    <p>
+                        Argentina, Colombia, Chile, República Dominicana, Guatemala, Costa Rica, Panamá, España, Perú, Ecuador, Venezuela.
+                    </p>
+                    <img src="/img/flags.png" alt="flags" style={{ "maxWidth": "100%"}} className="mt-5"/>
+                </TarjetaCapacitacionL>
             </Row>
         </Container>
     </section>
--- src/components/TarjetaCapacitacionL.jsx
+++ src/components/TarjetaCapacitacionL.jsx
@@ -1,18 +1,18 @@
 import { Col, Container, Row } from "react-bootstrap"
 
 // eslint-disable-next-line react/prop-types
-export const TarjetaCapacitacionR = ({imgNumber, img, children}) => {
+export const TarjetaCapacitacionL = ({imgNumber, img, children}) => {
   return (
     <Container className="tarjeta-capacitacion">
         <Row>
+            <Col className="imgL">
+                {img}
+            </Col>
             <Col>
-                <Row className="imgNumberL">
+                <Row className="imgNumberR">
                     {imgNumber}
                 </Row>
                 {children}
-            </Col>
-            <Col className="imgR" >
-                {img}
             </Col>
         </Row>
     </Container>
